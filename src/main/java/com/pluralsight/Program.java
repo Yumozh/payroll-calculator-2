76328e3fb90da1ef2cc01fa83c19e62ffc13163b
Yumozh <juliiamozhchil@gmail.com>
2026-04-21 11:03:18 -0500

    feat: read a document

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,4 +1,33 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
 public class Program {
+    public static void main(String[] args) {
+
+        try {
+            FileReader fileReader = new FileReader("employees.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line = bufferedReader.readLine();
+
+            while (line != null) {
+
+                line = bufferedReader.readLine();
+                String[] fields = line.split(Pattern.quote("|"));
+
+
+
+            }
+
+            bufferedReader.close();
+
+        } catch (IOException e) {
+            System.out.println("The file could not be read. Please make sure the file is available and not locked and then try again.");
+            e.printStackTrace();
+        }
+    }
 }
