e20d61c8f87546012df98b843239c7d6bf1e3bb0
Yumozh <juliiamozhchil@gmail.com>
2026-04-21 11:25:57 -0500

    feat: print all results

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -20,12 +20,14 @@ public class Program {
                 String[] fields = line.split(Pattern.quote("|"));
 
                 int employeeId = Integer.parseInt(fields[0].trim());
-                String name = fields[0];
+                String name = fields[1].trim();
                 double hoursWorked = Double.parseDouble(fields[2].trim());
                 double payRate = Double.parseDouble(fields[3].trim());
 
                 Employee currentEmployee = new Employee(employeeId, name, hoursWorked, payRate);
-                System.out.printf("Employee ID: %d\nEmployee name: %s\nEmployee gross pay: %.2f", currentEmployee.getEmployeeId(), currentEmployee.getName(), currentEmployee.getGrossPay(currentEmployee.getHoursWorked(), currentEmployee.getPayRate()));
+                double grossPay = currentEmployee.getGrossPay(currentEmployee.getHoursWorked(), currentEmployee.getPayRate());
+
+                System.out.printf("Employee ID: %d\nEmployee name: %s\nEmployee gross pay: $%.2f\n\n", currentEmployee.getEmployeeId(), currentEmployee.getName(), grossPay);
             }
 
             bufferedReader.close();
