958cea71717b9231d1abe8fd0db518c262dc7a9d
Yumozh <juliiamozhchil@gmail.com>
2026-04-21 11:20:59 -0500

    feat: assign values to variables from file

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -19,8 +19,13 @@ public class Program {
                 line = bufferedReader.readLine();
                 String[] fields = line.split(Pattern.quote("|"));
 
+                int employeeId = Integer.parseInt(fields[0].trim());
+                String name = fields[0];
+                double hoursWorked = Double.parseDouble(fields[2].trim());
+                double payRate = Double.parseDouble(fields[3].trim());
 
-
+                Employee currentEmployee = new Employee(employeeId, name, hoursWorked, payRate);
+                System.out.printf("Employee ID: %d\nEmployee name: %s\nEmployee gross pay: %.2f", currentEmployee.getEmployeeId(), currentEmployee.getName(), currentEmployee.getGrossPay(currentEmployee.getHoursWorked(), currentEmployee.getPayRate()));
             }
 
             bufferedReader.close();
